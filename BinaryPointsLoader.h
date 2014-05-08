299664b79e0e60e9076297037def650465967280
Alessandro Febretti <febret@gmail.com>
2014-05-08 17:31:51 -0500

    NEW paged point loading (WIP)

--- BinaryPointsLoader.h
+++ BinaryPointsLoader.h
@@ -1,15 +1,18 @@
 #ifndef _BINARY_POINTS_LOADER_H_
 #define _BINARY_POINTS_LOADER_H_
 
-#include <omega.h>
+//#include <omega.h>
 #include <cyclops/cyclops.h>
 
 // OSG
-#include <osg/Group>
-#include <osg/Vec3>
-#include <osg/Uniform>
-#include <osgDB/ReadFile>
-#include <osgDB/FileUtils>
+//#include <osg/Group>
+//#include <osg/Vec3>
+//#include <osg/Uniform>
+//#include <osgDB/ReadFile>
+//#include <osgDB/FileUtils>
+
+#include "BinaryPointsReader.h"
+
 
 using namespace omega;
 
---
+++ BinaryPointsReader.h
@@ -0,0 +1,38 @@
+#ifndef _BINARY_POINTS_READER_H_
+#define _BINARY_POINTS_READER_H_
+
+#include <omega.h>
+
+// OSG
+#include <osg/Group>
+#include <osg/Vec3>
+#include <osg/Uniform>
+#include <osgDB/ReadFile>
+#include <osgDB/FileUtils>
+#include <osgDB/ReaderWriter>
+#include <osg/ValueObject>
+
+using namespace omega;
+
+class BinaryPointsReader: public osgDB::ReaderWriter
+{
+public:
+    BinaryPointsReader()
+    {
+        supportsExtension("xyzb", "XYZ binary");
+    }
+
+    const char* className() const { return "Binary points reader"; }
+
+    virtual ReadResult readNode(const std::string& filename, const Options*) const;
+
+private:
+    void readXYZ(
+        const String& filename,
+        int readStartP, int readLengthP, int decimation,
+        osg::Vec3Array* points, osg::Vec4Array* colors,
+        int* numPoints,
+        Vector4f* rgbamin,
+        Vector4f* rgbamax) const;
+};
+#endif
