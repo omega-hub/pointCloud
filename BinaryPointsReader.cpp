6d3673387bfa527e9e1b46d67f070d6a59d51316
Alessandro Febretti <febret@gmail.com>
2014-05-22 17:31:33 -0500

    NEW: -F option to read single-precision binary point cloud files

--- BinaryPointsReader.cpp
+++ BinaryPointsReader.cpp
@@ -16,6 +16,8 @@ osgDB::ReaderWriter::ReadResult BinaryPointsReader::readNode(const std::string& 
 
     String actualFilename = filename;
 
+    bool useSinglePrecision = false;
+
     // Check the options to see if we should read a subsection of the file
     // and / or use decimation.
     int readStartP = 0;
@@ -33,6 +35,7 @@ osgDB::ReaderWriter::ReadResult BinaryPointsReader::readNode(const std::string& 
         ah.newNamedInt('l', "length", "length", "number of records to read %", readLengthP);
         ah.newNamedInt('d', "decimation", "decimation", "read decimation", decimation);
         ah.newNamedInt('b', "batch-size", "batch size", "batch size", batchSize);
+        ah.newFlag('F', "float", "Use single precision floating point", useSinglePrecision);
         ah.process(o->getOptionString().c_str());
     }
 
@@ -77,13 +80,24 @@ osgDB::ReaderWriter::ReadResult BinaryPointsReader::readNode(const std::string& 
         Vector4f rgbamin = Vector4f(maxf, maxf, maxf, maxf);
         Vector4f rgbamax = Vector4f(minf, minf, minf, minf);
 
-        readXYZ(path,
-            readStartP, readLengthP, decimation,
-            verticesP, verticesC,
-            &numPoints,
-            &rgbamin,
-            &rgbamax);
-
+        if(useSinglePrecision)
+        {
+            readXYZ<float>(path,
+                readStartP, readLengthP, decimation,
+                verticesP, verticesC,
+                &numPoints,
+                &rgbamin,
+                &rgbamax);
+        }
+        else
+        {
+            readXYZ<double>(path,
+                readStartP, readLengthP, decimation,
+                verticesP, verticesC,
+                &numPoints,
+                &rgbamin,
+                &rgbamax);
+        }
         // create geometry and geodes to hold the data
         osg::Geode* geode = new osg::Geode();
         geode->setCullingActive(true);
@@ -142,125 +156,3 @@ osgDB::ReaderWriter::ReadResult BinaryPointsReader::readNode(const std::string& 
     return ReadResult();
 }
 
-///////////////////////////////////////////////////////////////////////////////
-void BinaryPointsReader::readXYZ(
-    const String& filename,
-    int readStartP, int readLengthP, int decimation,
-    osg::Vec3Array* points, osg::Vec4Array* colors,
-    int* numPoints,
-    Vector4f* rgbamin,
-    Vector4f* rgbamax) const
-{
-    osg::Vec3f point;
-    osg::Vec4f color(1.0f, 1.0f, 1.0f, 1.0f);
-
-    // Default record size = 7 doubles (X,Y,Z,R,G,B,A)
-    int numFields = 7;
-    size_t recordSize = sizeof(double)* numFields;
-
-    FILE* fin = fopen(filename.c_str(), "rb");
-
-    // How many records are in the file?
-    fseek(fin, 0, SEEK_END);
-    long endpos = ftell(fin);
-    fseek(fin, 0, SEEK_SET);
-    long numRecords = endpos / recordSize;
-    long readStart = numRecords * readStartP / 100;
-    long readLength = numRecords * readLengthP / 100;
-
-    if(decimation <= 0) decimation = 1;
-    if(readStart != 0)
-    {
-        fseek(fin, readStart * recordSize, SEEK_SET);
-    }
-
-    // Adjust read length.
-    if(readLength == 0 || readStart + readLength > numRecords)
-    {
-        readLength = numRecords - readStart;
-    }
-
-    ofmsg("BinaryPointsLoader: reading records %1% - %2% of %3% (decimation %4%) of %5%",
-        %readStart % (readStart + readLength) % numRecords %decimation %filename);
-
-    // Read in data
-    double* buffer = (double*)malloc(recordSize * readLength / decimation);
-    if(buffer == NULL)
-    {
-        oferror("BinaryPointsLoader::readXYZ: could not allocate %1% bytes",
-            % (recordSize * readLength / decimation));
-        return;
-    }
-
-    int ne = readLength / decimation;
-
-    // Read data
-    // If data is not decimated, read it in one go.
-    if(decimation == 1)
-    {
-        size_t size = fread(buffer, recordSize, readLength, fin);
-    }
-    else
-    {
-        int j = 0;
-        for(int i = 0; i < ne; i++)
-        {
-            // Read one record
-            size_t size = fread(&buffer[j], recordSize, 1, fin);
-            // Skip ahead decimation - 1 records.
-            fseek(fin, recordSize * (decimation - 1), SEEK_CUR);
-            j += numFields;
-        }
-    }
-
-    points->reserve(ne);
-    colors->reserve(ne);
-
-    int j = 0;
-    for(int i = 0; i < ne; i++)
-    {
-        point[0] = buffer[i * numFields];
-        point[1] = buffer[i * numFields + 1];
-        point[2] = buffer[i * numFields + 2];
-
-        color[0] = buffer[i * numFields + 3];
-        color[1] = buffer[i * numFields + 4];
-        color[2] = buffer[i * numFields + 5];
-        color[3] = buffer[i * numFields + 6];
-
-        points->push_back(point);
-        colors->push_back(color);
-
-        // Update data bounds and number of points
-        *numPoints = *numPoints + 1;
-        for(int j = 0; j < 4; j++)
-        {
-            if(color[j] < (*rgbamin)[j]) (*rgbamin)[j] = color[j];
-            if(color[j] > (*rgbamax)[j]) (*rgbamax)[j] = color[j];
-        }
-    }
-
-    fclose(fin);
-    free(buffer);
-}
-
-/*
-///////////////////////////////////////////////////////////////////////////////
-bool BinaryPointsLoader::load(ModelAsset* model)
-{
-    osg::ref_ptr<osg::Group> group = new osg::Group();
-
-	bool result = loadFile(model, group);
-
-    if(result)
-    {
-		//osg::Geode* points;
-	    //points = group->getChild(0)->asGeode();
-	    
-		model->nodes.push_back(group->asGroup());
-
-	    //group->removeChild(0, 1);
-    }
-    return result;
-}
-*/
--- BinaryPointsReader.h
+++ BinaryPointsReader.h
@@ -27,6 +27,7 @@ public:
     virtual ReadResult readNode(const std::string& filename, const Options*) const;
 
 private:
+    template<typename T>
     void readXYZ(
         const String& filename,
         int readStartP, int readLengthP, int decimation,
@@ -35,4 +36,107 @@ private:
         Vector4f* rgbamin,
         Vector4f* rgbamax) const;
 };
+
+///////////////////////////////////////////////////////////////////////////////
+template<typename T>
+void BinaryPointsReader::readXYZ(
+    const String& filename,
+    int readStartP, int readLengthP, int decimation,
+    osg::Vec3Array* points, osg::Vec4Array* colors,
+    int* numPoints,
+    Vector4f* rgbamin,
+    Vector4f* rgbamax) const
+{
+    osg::Vec3f point;
+    osg::Vec4f color(1.0f, 1.0f, 1.0f, 1.0f);
+
+    // Default record size = 7 doubles (X,Y,Z,R,G,B,A)
+    int numFields = 7;
+    size_t recordSize = sizeof(T)* numFields;
+
+    FILE* fin = fopen(filename.c_str(), "rb");
+
+    // How many records are in the file?
+    fseek(fin, 0, SEEK_END);
+    long endpos = ftell(fin);
+    fseek(fin, 0, SEEK_SET);
+    long numRecords = endpos / recordSize;
+    long readStart = numRecords * readStartP / 100;
+    long readLength = numRecords * readLengthP / 100;
+
+    if(decimation <= 0) decimation = 1;
+    if(readStart != 0)
+    {
+        fseek(fin, readStart * recordSize, SEEK_SET);
+    }
+
+    // Adjust read length.
+    if(readLength == 0 || readStart + readLength > numRecords)
+    {
+        readLength = numRecords - readStart;
+    }
+
+    ofmsg("BinaryPointsLoader: reading records %1% - %2% of %3% (decimation %4%) of %5%",
+        %readStart % (readStart + readLength) % numRecords %decimation %filename);
+
+    // Read in data
+    T* buffer = (T*)malloc(recordSize * readLength / decimation);
+    if(buffer == NULL)
+    {
+        oferror("BinaryPointsLoader::readXYZ: could not allocate %1% bytes",
+            % (recordSize * readLength / decimation));
+        return;
+    }
+
+    int ne = readLength / decimation;
+
+    // Read data
+    // If data is not decimated, read it in one go.
+    if(decimation == 1)
+    {
+        size_t size = fread(buffer, recordSize, readLength, fin);
+    }
+    else
+    {
+        int j = 0;
+        for(int i = 0; i < ne; i++)
+        {
+            // Read one record
+            size_t size = fread(&buffer[j], recordSize, 1, fin);
+            // Skip ahead decimation - 1 records.
+            fseek(fin, recordSize * (decimation - 1), SEEK_CUR);
+            j += numFields;
+        }
+    }
+
+    points->reserve(ne);
+    colors->reserve(ne);
+
+    int j = 0;
+    for(int i = 0; i < ne; i++)
+    {
+        point[0] = buffer[i * numFields];
+        point[1] = buffer[i * numFields + 1];
+        point[2] = buffer[i * numFields + 2];
+
+        color[0] = buffer[i * numFields + 3];
+        color[1] = buffer[i * numFields + 4];
+        color[2] = buffer[i * numFields + 5];
+        color[3] = buffer[i * numFields + 6];
+
+        points->push_back(point);
+        colors->push_back(color);
+
+        // Update data bounds and number of points
+        *numPoints = *numPoints + 1;
+        for(int j = 0; j < 4; j++)
+        {
+            if(color[j] < (*rgbamin)[j]) (*rgbamin)[j] = color[j];
+            if(color[j] > (*rgbamax)[j]) (*rgbamax)[j] = color[j];
+        }
+    }
+
+    fclose(fin);
+    free(buffer);
+}
 #endif
